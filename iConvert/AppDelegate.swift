1e2a261020ffdc09b6883df77603f089c0b0829c
Denny Mathew <denny.mathew@cabotsolutions.com>
2018-08-22 00:33:32 +0530

    Initial Commit

---
+++ iConvert/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iConvert
+//
+//  Created by Denny Mathew on 8/22/18.
+//  Copyright © 2018 Cabot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iConvert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iConvert
+//
+//  Created by Denny Mathew on 8/22/18.
+//  Copyright © 2018 Cabot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
